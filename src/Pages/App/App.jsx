cc32d9848090058b83e54dfe6d54a229e72cabfd
SeksiBoiDom <ndominic56@gmail.com>
2023-05-22 13:27:09 -0400

    add notes

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -1,31 +1,44 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
-import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
-import NavBar from '../../components/NavBar/NavBar';
+import { useEffect } from 'react';
 import './App.css';
+import AuthPage from '../AuthPage/AuthPage';
+import NoteForm from '../../components/NoteForm/NoteForm';
+import NoteHistoryPage from '../NoteHistoryPage/NoteHistoryPage';
+import NavBar from '../../components/NavBar/NavBar';
+import NewNotePage from '../NewNotePage/NewNotePage';
 
-function App() {
+export default function App() {
   const [user, setUser] = useState(getUser());
+  const [notes, setNote] = useState([])
+  const [showNotes, setShowNotes] = useState(true)
+
+  useEffect(() => {
+    const storedNote = localStorage.getItem('notes');
+    if (storedNote) {
+      setNote(JSON.parse(storedNote));
+    }
+  }, []);
+
+  function addNote(note) {
+    const updatedNote = [...notes, note];
+    setNote(updatedNote);
+    localStorage.setItem('notes', JSON.stringify(updatedNote));
+  }
 
   return (
     <main className="App">
       { user ?
-        <>
-          <NavBar user={user} setUser={setUser} />
-          <Routes>
-            {/* Route components in here */}
-            <Route path="/orders/new" element={<NewOrderPage />} />
-            <Route path="/orders" element={<OrderHistoryPage />} />
-          </Routes>
-        </>
-        :
-        <AuthPage setUser={setUser} />
+          <>
+            <NavBar user={user} setUser={setUser} />
+            <NoteForm addNote={addNote} />
+            <h1>Notes</h1>
+            { showNotes && <NoteHistoryPage user={user} notes={notes} />}
+          </>
+          :
+          <AuthPage setUser={setUser} />
       }
     </main>
   );
 }
-
-export default App;
