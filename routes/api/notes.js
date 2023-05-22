d02345e6a8de6b48ff1c2cfedf62c9d3fed7344b
SeksiBoiDom <ndominic56@gmail.com>
2023-05-22 13:33:26 -0400

    add notes

---
+++ routes/api/notes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const notesCtrl = require('../../controllers/api/users');
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
+
+// All paths start with '/api/users'
+
+// POST /api/users (create a user - sign up)
+router.post('/notes', ensureLoggedIn, notesCtrl.create);
+// POST /api/users/login
+router.delete('/notes/:id', ensureLoggedIn, notesCtrl.delete);
+// GET /api/users/check-token
+// router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
+
+module.exports = router;
